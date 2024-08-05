aff98f46555b9b21728b547203d954c83a569976
LSB10000 <lsb6@cin.ufpe.br>
2024-08-05 18:31:49 -0300

    Create audio.py

---
+++ audio.py
@@ -0,0 +1,6 @@
+import pygame
+
+pygame.mixer.init()
+torcida = pygame.mixer.music.load('audio/sons_de_torcida.wav')
+pygame.mixer.music.play(-1)
+kick_sound = pygame.mixer.Sound('audio/ball_kick.wav')
