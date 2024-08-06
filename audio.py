31ddd874ae267d41c2bf1c9529ad848b9f247065
LSB10000 <lsb6@cin.ufpe.br>
2024-08-06 11:06:10 -0300

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.mixer.init()
-torcida = pygame.mixer.music.load('audio/sons_de_torcida.wav')
+torcida = pygame.mixer.music.load('./audio/sons_de_torcida.wav')
 pygame.mixer.music.play(-1)
-kick_sound = pygame.mixer.Sound('audio/ball_kick.wav')
-whistle_sound = pygame.mixer.Sound('audio/whistle.wav')
+kick_sound = pygame.mixer.Sound('./audio/ball_kick.wav')
+whistle_sound = pygame.mixer.Sound('./audio/whistle.wav')
